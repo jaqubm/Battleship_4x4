01cca5962f9c1394355531de5701481590f88b12
Michal-Smiechowicz <240817@edu.p.lodz.pl>
2023-05-28 13:52:47 +0200

    Drag and drop w obrębie planszy.

---
+++ src/main/java/com/battleship_4x4/Ship.java
@@ -0,0 +1,63 @@
+package com.battleship_4x4;
+
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import static com.battleship_4x4.MainMenu.TILE_SIZE;
+
+public class Ship extends StackPane {
+    //private final ShipType type;
+    private double mouseX, mouseY;
+    private double oldX, oldY; //previous position of the piece
+
+    /*public ShipType getType() {
+        return this.type;
+    }*/
+
+    public double getOldX() {
+        return oldX;
+    }
+
+    public double getOldY() {
+        return oldY;
+    }
+
+    public Ship(int x, int y) { //piece constructor
+        //this.type = type;
+
+        move(x, y); //now new position of the piece
+
+        Rectangle ship = new Rectangle(TILE_SIZE * 0.3, TILE_SIZE * 0.3);
+        ship.setFill( Color.WHITESMOKE);
+        ship.setStroke(Color.SADDLEBROWN);
+        ship.setStrokeWidth(TILE_SIZE * 0.03);
+        ship.setTranslateX((TILE_SIZE - TILE_SIZE * 0.3 * 2) / 2);
+        ship.setTranslateY((TILE_SIZE - TILE_SIZE * 0.3 * 2) / 2);
+
+
+        getChildren().add(ship);
+
+        setOnMousePressed(e ->
+        { //tracking where the mouse is
+            mouseX = e.getSceneX();
+            mouseY = e.getSceneY();
+        });
+        setOnMouseDragged(e -> {
+            relocate(e.getSceneX() - mouseX + oldX, e.getSceneY() - mouseY + oldY);
+        });
+    }
+
+
+
+    public void move (int x, int y) {
+        oldX = x * TILE_SIZE;
+        oldY = y * TILE_SIZE;
+
+        relocate(oldX, oldY);
+    }
+
+    public void abortMove() {
+        relocate(oldX, oldY);
+    }
+}
---
+++ src/main/java/com/battleship_4x4/Tile.java
@@ -0,0 +1,31 @@
+package com.battleship_4x4;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import static com.battleship_4x4.MainMenu.TILE_SIZE;
+public class Tile extends Rectangle
+{
+    private Ship ship;
+
+    public boolean hasShip()
+    {
+        return ship != null;
+    }
+    public Ship getShip()
+    {
+        return this.ship;
+    }
+    public void setShip(Ship ship)
+    {
+        this.ship = ship;
+    }
+    public Tile(boolean light, int x, int y)
+    {
+        setWidth(TILE_SIZE);
+        setHeight(TILE_SIZE);
+
+        relocate(x * TILE_SIZE, y * TILE_SIZE);
+
+        setFill(light ? Color.BLUE : Color.CYAN);
+    }
+}
