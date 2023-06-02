bdf1921e672f56992d1523cd9015beabd0eebf95
SebixR <sebarog@gmail.com>
2023-06-02 11:53:28 +0200

    Dodano możliwość poruszania statkami w obrębie planszy, wraz z obsługą kolizji statków

--- src/main/java/com/battleship_4x4/Controller.java
+++ src/main/java/com/battleship_4x4/Controller.java
@@ -17,11 +17,14 @@ public class Controller implements Initializable {
     @FXML
     private AnchorPane boardPane;
     private GridHandler backgroundGridHandler;
+    private DraggableMakerGrid draggableMakerGrid;
     public List<Ship> ships = new ArrayList<>();
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        draggableMakerGrid = new DraggableMakerGrid(boardPane.getPrefWidth(), boardPane.getPrefHeight(), gridSize, boardPane);
 
         backgroundGridHandler = new GridHandler(boardPane.getPrefWidth(), boardPane.getPrefHeight(), gridSize, boardPane);
         backgroundGridHandler.createGrid();
@@ -29,20 +32,24 @@ public class Controller implements Initializable {
         Ship ship2 = new Ship(gridSize, 2);
         ships.add(ship2);
         boardPane.getChildren().add(ship2.getRectangle());
+        draggableMakerGrid.makeDraggable(ship2, ships);
 
         Ship ship3 = new Ship(gridSize, 3);
         ships.add(ship3);
         ship3.move(0, 1);
         boardPane.getChildren().add(ship3.getRectangle());
+        draggableMakerGrid.makeDraggable(ship3, ships);
 
         Ship ship4 = new Ship(gridSize, 4);
         ships.add(ship4);
         ship4.move(0, 2);
         boardPane.getChildren().add(ship4.getRectangle());
+        draggableMakerGrid.makeDraggable(ship4, ships);
 
         Ship ship5 = new Ship(gridSize, 5);
         ships.add(ship5);
         ship5.move(0, 3);
         boardPane.getChildren().add(ship5.getRectangle());
+        draggableMakerGrid.makeDraggable(ship5, ships);
     }
 }
