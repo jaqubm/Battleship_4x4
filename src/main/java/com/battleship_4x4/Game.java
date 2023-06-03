de031d156c3acf52b292da0533a929ef7f86e2a5
Jakub Matuszak <jaqobgm@gmail.com>
2023-06-03 17:41:04 +0200

    Poprawiony render plansz

--- src/main/java/com/battleship_4x4/Game.java
+++ src/main/java/com/battleship_4x4/Game.java
@@ -14,25 +14,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class Game extends Application {
+public class Game extends Application implements Initializable {
 
     @FXML
-    private AnchorPane boardPane_1;
+    private AnchorPane boardPane1;
     @FXML
-    private AnchorPane boardPane_2;
+    private AnchorPane boardPane2;
     @FXML
-    private AnchorPane boardPane_3;
+    private AnchorPane boardPane3;
     @FXML
-    private AnchorPane boardPane_4;
+    private AnchorPane boardPane4;
     public List<Ship> ships = new ArrayList<>();
 
-    public void Initialize() {
-        int gridSize = 32;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        int gridSize = (int)(boardPane1.getPrefWidth() / 8);
 
-        GridHandler backgroundGrid_1 = new GridHandler(boardPane_1.getPrefWidth(), boardPane_1.getPrefHeight(), gridSize, boardPane_1);
-        GridHandler backgroundGrid_2 = new GridHandler(boardPane_2.getPrefWidth(), boardPane_2.getPrefHeight(), gridSize, boardPane_2);
-        GridHandler backgroundGrid_3 = new GridHandler(boardPane_3.getPrefWidth(), boardPane_3.getPrefHeight(), gridSize, boardPane_3);
-        GridHandler backgroundGrid_4 = new GridHandler(boardPane_4.getPrefWidth(), boardPane_4.getPrefHeight(), gridSize, boardPane_4);
+        GridHandler backgroundGrid_1 = new GridHandler(boardPane1.getPrefWidth(), boardPane1.getPrefHeight(), gridSize, boardPane1);
+        GridHandler backgroundGrid_2 = new GridHandler(boardPane2.getPrefWidth(), boardPane2.getPrefHeight(), gridSize, boardPane2);
+        GridHandler backgroundGrid_3 = new GridHandler(boardPane3.getPrefWidth(), boardPane3.getPrefHeight(), gridSize, boardPane3);
+        GridHandler backgroundGrid_4 = new GridHandler(boardPane4.getPrefWidth(), boardPane4.getPrefHeight(), gridSize, boardPane4);
 
         backgroundGrid_1.createGrid();
         backgroundGrid_2.createGrid();
@@ -41,31 +42,29 @@ public class Game extends Application {
 
         Ship ship2 = new Ship(gridSize, 2);
         ships.add(ship2);
-        boardPane_1.getChildren().add(ship2.getRectangle());
+        boardPane1.getChildren().add(ship2.getRectangle());
 
         Ship ship3 = new Ship(gridSize, 3);
         ships.add(ship3);
         ship3.move(0, 1);
-        boardPane_1.getChildren().add(ship3.getRectangle());
+        boardPane1.getChildren().add(ship3.getRectangle());
 
         Ship ship4 = new Ship(gridSize, 4);
         ships.add(ship4);
         ship4.move(0, 2);
-        boardPane_1.getChildren().add(ship4.getRectangle());
+        boardPane1.getChildren().add(ship4.getRectangle());
 
         Ship ship5 = new Ship(gridSize, 5);
         ships.add(ship5);
         ship5.move(0, 3);
-        boardPane_1.getChildren().add(ship5.getRectangle());
+        boardPane1.getChildren().add(ship5.getRectangle());
     }
 
     @Override
     public void start(Stage stage) throws IOException
     {
-        Initialize();
         FXMLLoader fxmlLoader = new FXMLLoader(Setup.class.getResource("game.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-
         stage.setTitle("Setup");
         stage.setResizable(false);
         stage.setScene(scene);
